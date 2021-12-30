2cf2ca5f0ee78e77c77a6c17e1e732ff8360f4f5
Aravind Reddy V <aravind.reddy@iiitb.org>
2021-12-30 09:43:51 +0530

    Add error reporting and exit on wrong kernel

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+set -Eeuo pipefail
+shopt -s inherit_errexit
+
+error() {
+    local line_no="$1"
+    local func_name="$2"
+    local code="${3:-1}"
+
+    # https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
+    local C_FGRED='\033[1;31m'
+    local C_NC='\033[0m' # No Color
+
+    echo -e "An ${C_FGRED}ERROR${C_NC} occurred in function ${C_FGRED}${func_name}${C_NC} on or near line ${C_FGRED}$line_no${C_NC}: ${C_FGRED}$BASH_COMMAND${C_NC}"
+    echo -e "The program will exit with exit-code ${C_FGRED}${code}${C_NC}\n"
+
+    read -p "Please review the error and press any key to exit this program " -n 1 -r
+    exit "${code}"
+}
+
+trap 'error $LINENO ${FUNCNAME-main}' ERR
+
 function setStatus () {
   if { [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; } then
     tmux set status-right "Status: $1"   
@@ -8,6 +29,19 @@ function setStatus () {
   echo "********** ----- Status: $1 ----- ***********"
   echo "$1" >> 'nexmoncsi_status.log'
 }
+
+case "$(uname -r)" in
+    "5.4."*)
+        echo "Running on kernel 5.4.y"
+    ;;
+
+    *)
+        echo "This script is designed to be run on kernel version 5.4.51, but you seem to be running version $(uname -r)."
+        echo "Please use Raspbian Buster Lite 2020-08-20 as indicated in the ReadMe to install Nexmon_CSI."
+        read -p "Please review the error and press any key to exit this program " -n 1 -r
+        exit 1
+    ;;
+esac
 
 setStatus "Removing wpasupplicant"
 apt remove wpasupplicant -y
